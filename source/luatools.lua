c1c07f95a2079aec614f02cc7e957df84d1243f2
Max Chernoff <49086429+gucci-on-fleek@users.noreply.github.com>
2024-05-12 02:07:33 -0600

    Module additions

--- source/luatools.lua
+++ source/luatools.lua
@@ -1,14 +1,50 @@
 --=-----------------------------
 --= \section{Initialization} ---
 --=-----------------------------
+--=
+--= Before we can do anything else, we need to run some various initialization
+--= steps.
 
---= Make sure we haven't already been loaded.
+-- Make sure we haven't already been loaded.
 
 if luatools then
     return luatools
 end
 
---= First, we define a global table \typ{luatools} to hold all of our defined
+
+--= We're going to use some of the \ConTeXt{} \typ{lualibs} functions quite a
+--= bit, so we'll save some (private) local versions of them here.
+
+--- @diagnostic disable - The typechecker hasn't heard of these functions, so we
+---                       disable it for now.
+
+--= Sets the \typ{__index} metamethod of a table.
+--- @overload fun(t: table, f: function): table
+--- @overload fun(f: function): table
+local setmetatableindex = table.setmetatableindex
+
+--= Does \lua{{ "a", "b", "c" }} \to \lua{{ a = 1, b = 2, c = 3 }}
+--- @generic K,V - -
+--- @type fun(t: table<K, V>): table<V, K>
+local table_swapped = table.swapped
+
+--= Inserts a value into a table.
+local insert = table.insert
+
+--= Appends a table to another table, in place.
+--- @type fun(a: table, b: table): table
+local append = table.append
+
+--= Concatenates a table into a string.
+local concat = table.concat
+
+--= Gets the character at a given codepoint.
+local utf_char = utf8.char
+
+--- @diagnostic enable - -
+
+
+--= Now, we define a global table \typ{luatools} to hold all of our defined
 --= functions.
 
 --- @class luatools -               The module class.
@@ -152,6 +188,7 @@ local lt
 ---                              \typ{YYYY-MM-DD}.
 --- @field description? string   A short description of the module.
 --- @field debug?       boolean  Whether to show debug messages.
+--- @field expl?        boolean  Whether to use \LaTeX{} expl3 conventions.
 --- @field self?        luatools The module root.
 
 --- @param  config   luatools.config A table containing module-local data.
@@ -164,6 +201,7 @@ function luatools.init(config)
 
     config.ns    = config.ns or config.name
     config.debug = config.debug or false
+    config.expl  = config.expl or false
 
     -- Create a new table
     local self = { config = config }
@@ -232,6 +270,8 @@ end
 --=-----------------------
 --= \section{Messages} ---
 --=-----------------------
+--=
+--= Here, we define some functions used for printing messages.
 
 --- @class luatools.msg - A table containing message functions.
 --- @field self luatools  The module root.
@@ -248,7 +288,7 @@ local CONSOLE = luatex_lmtx("term", "terminal")
 --- @param  ... string The messages to print.
 --- @return nil -      -
 function luatools.msg:console(...)
-    local msg = table.concat({...}, "\t")
+    local msg = concat({...}, "\t")
     write_nl(CONSOLE, msg)
 end
 
@@ -262,7 +302,7 @@ local LOG_FILE = luatex_lmtx("log", "logfile")
 --- @param  ... string The messages to print.
 --- @return nil -      -
 function luatools.msg:log(...)
-    local msg = table.concat({...}, "\t")
+    local msg = concat({...}, "\t")
     write_nl(LOG_FILE, msg)
 end
 
@@ -273,10 +313,16 @@ end
 
 local TERM_AND_LOG = luatex_lmtx("term and log", "terminal_and_logfile")
 
---- @param  ... string The messages to print.
---- @return nil -      -
+--- @param  ... any The messages to print.
+--- @return nil -   -
 function luatools.msg:print(...)
-    local msg = table.concat({...}, "\t")
+    -- Make sure that all messages are strings
+    local msgs = {...}
+    for i, msg in ipairs(msgs) do
+        msgs[i] = tostring(msg)
+    end
+
+    local msg = concat(msgs, "\t")
     write_nl(TERM_AND_LOG, msg)
 end
 
@@ -286,13 +332,13 @@ end
 --= Prints a message to both the console and the log file, but only if debugging
 --= is enabled.
 
---- @param  ... string The messages to print.
---- @return nil -      -
+--- @param  ... any The messages to print.
+--- @return nil -   -
 function luatools.msg:debug(...)
     self = self.self
 
     if self.config.debug then
-        self.msg:print(...)
+        self.msg:print(self.config.name, ...)
     end
 end
 
@@ -401,8 +447,337 @@ function luatools.msg:error(msg)
     self.msg:error(msg)
 end
 
+
+--=------------------------
+--= \section{Utilities} ---
+--=------------------------
+--=
+--= Here, we define some general-purpose utility functions.
+
+--- @class luatools.util - A table containing utility functions.
+--- @field self luatools  The module root.
+luatools.util = {}
+
+
+--= \subsection{\typ{luatools.util:memoized}}
+--=
+--= Memoizes a function call/table index.
+
+--- @param  func function<any, any> The function to memoize
+--- @return table -                 The “function”
+function luatools.util:memoized(func)
+    return setmetatable({}, { __index = function(cache, key)
+        local ret = func(key, cache)
+        cache[key] = ret
+        return ret
+    end,  __call = function(self, arg)
+        return self[arg]
+    end })
+end
+
+
+--=----------------------------
+--= \section{Node Handling} ---
+--=----------------------------
+--=
+--= Here, we define some functions for working with \LuaTeX{} nodes.
+
+--- @class node -         \LuaTeX{} node “userdata” objects.
+--- @field id      number The node's type.
+--- @field subtype number The node's subtype.
+--- @field next?   node   The next node in the list.
+--- @field prev?   node   The previous node in the list.
+
+--- @class luatools.node -       A table containing node functions.
+--- @field self luatools         The module root.
+--- @field [string] fun(...):... A wrapper around the \LuaTeX{} \typ{node}
+---                              functions.
+luatools.node = setmetatableindex(function(t, k)
+--= LuaMeta\TeX{} removes underscores from most of its \typ{node} functions, so
+--= we'll try removing any underscores if we can't find the function.
+    local v = node[k] or node[k:gsub("_", "")]
+    t[k] = v
+    return v
+end)
+
+
+--= \subsection{\typ{luatools.node:type}}
+--=
+--= Gets the type and subtype of a node as pair of strings.
+
+local node_id_to_type = node.types()
+local node_subid_to_subtype = {}
+
+for id, name in pairs(node_id_to_type) do
+    node_subid_to_subtype[id] = node.subtypes(name)
+end
+
+--= LuaMeta\TeX{} gives different names to some node types, and these names are
+--= generally much more sensible, so we'll use the same names for \LuaTeX{} too.
+if not lt.fmt.lmtx then
+    local node_type_to_id = table_swapped(node_id_to_type)
+
+    -- Inserts
+    local insert_id = node_type_to_id["ins"]
+    node_id_to_type[insert_id] = "insert"
+
+    -- Paragraphs
+    local paragraph_id = node_type_to_id["local_par"]
+    node_id_to_type[paragraph_id] = "par"
+
+    -- Some other names are renamed too, but they're rarely used so we'll ignore
+    -- them.
+end
+
+
+--- @param  n node           The node to get the type of.
+--- @return string,string? - The type and subtype of the node.
+function luatools.node:type(n)
+    local type = node_id_to_type[n.id]
+    local subtypes = node_subid_to_subtype[n.id]
+
+    local subtype
+    if subtypes and n.subtype then
+        subtype = subtypes[n.subtype]
+    end
+
+    return type, subtype
+end
+
+
+--= \subsection{\typ{luatools.node:is}}
+--=
+--= Checks if a node is the specified type or subtype.
+
+--- @param  n     node   The node to check.
+--- @param  query string The type or subtype to check against.
+--- @return boolean -    Whether the node is of the specified type or subtype.
+function luatools.node:is(n, query)
+    self = self.self
+
+    local type, subtype = self.node:type(n)
+
+    return query == type or query == subtype
+end
+
+
+--= \subsection{\typ{luatools.node:new}}
+--=
+--= Creates a new node with the specified parameters.
+
+--- @class _node_new -     The node creation parameters.
+--- @field type     string The type of the new node.
+--- @field subtype? string The subtype of the new node.
+--- @field [string] any    Any additional fields to set on the new node.
+
+--- @param  params _node_new The parameters for the new node.
+--- @return node   -         The new node.
+function luatools.node:new(params)
+    -- Create the new node
+    local n = node.new(params.type, params.subtype)
+
+    -- Set any additional fields
+    params.type = nil
+    params.subtype = nil
+    for k, v in pairs(params) do
+        n[k] = v
+    end
+
+    return n
+end
+
+
+--= \subsection{\typ{luatools.node:join}}
+--=
+--= Joins a list of nodes into a node list.
+
+--- @param  nodes node[] The nodes to join.
+--- @return node  head   The head of the new node list.
+function luatools.node:join(nodes)
+    local head = nodes[1]
+
+    -- Add the `next` pointers to each node
+    for i = 1, #nodes - 1 do
+        nodes[i].next = nodes[i + 1]
+    end
+
+    -- Add the `prev` pointers to each node
+    node.slide(head)
+
+    return head
+end
+
+
+--= \subsection{\typ{luatools.node:replace}}
+--=
+--= Replaces a specified node in a node list with another node.
+
+--- @param  head    node The head of the list that contains \typ{find}
+--- @param  find    node The node to remove
+--- @param  replace node The node to insert
+--- @return node    head The new head of the list
+function luatools.node:replace(head, find, replace)
+    self = self.self
+
+    -- Remove `find` from the list
+    local head, current = self.node.remove(head, find)
+
+    -- Insert `replace` in its place
+    head, replace = self.node.insert_before(head, current, replace)
+
+    return head
+end
+
+
+--= \subsection{\typ{luatools.node:get_chars}}
+--=
+--= Converts the contents of a node list to a string.
+
+--- @param  head node The head of the list to convert.
+--- @return string -  The contents of the list as a string.
+function luatools.node:get_chars(head)
+    self = self.self
+
+    local chars = {}
+
+    for n in self.node.traverse(head) do
+        if self.node:is(n, "glyph") then
+            insert(chars, utf_char(n.char))
+        elseif self.node:is(n, "glue") then
+            insert(chars, " ")
+        elseif n.list or n.replace then
+            insert(chars, self.node:get_chars(n.list or n.replace))
+        end
+    end
+
+    return concat(chars)
+end
+
+
+--= \subsection{\typ{luatools.node:get_next}}
+--=
+--= Gets the next node in a list that matches the specified criteria.
+
+--- @class _node_get_next -                The criteria to search for.
+--- @field is string                       The type of the node to search for.
+--- @field direction? "forward"|"backward" The direction to search in.
+
+--- @param  head  node              The head of the list to search.
+--- @param  criteria _node_get_next The criteria to search for.
+--- @return node? -                 The first node that matches the criteria.
+function luatools.node:get_next(head, criteria)
+    self = self.self
+
+    local is = criteria.is
+    local direction = criteria.direction or "forward"
+
+    if direction == "forward" then
+        -- Use the standard traverser, but stop at the first match
+        for n in self.node.traverse(head) do
+            if self.node:is(n, is) then
+                return n
+            end
+        end
+    elseif direction == "backward" and self.fmt.lmtx then
+        -- LuaMetaTeX has a builtin reverse traversal function
+        for n in self.node.traverse(head, true) do
+            if self.node:is(n, is) then
+                return n
+            end
+        end
+    elseif direction == "backward" and not self.fmt.lmtx then
+        -- For LuaTeX, we need to manually traverse the list in reverse
+        local n = head
+        while n do
+            if self.node:is(n, is) then
+                return n
+            end
+            n = n.prev
+        end
+    else
+        lt.msg:error("Invalid direction: " .. direction)
+    end
+end
+
+
+--= \subsection{\typ{luatools.node:map}}
+--=
+--= Maps a function over a list of nodes.
+
+--- @param  head node                 The head of the list to map over.
+--- @param  is   string|boolean       The type of nodes to map over (`true` for
+---                                   all).
+--- @param  func fun(n:node):...:node The function to apply to each node.
+--- @return node head                 The new head of the list.
+function luatools.node:map(head, is, func)
+    self = self.self
+
+    -- Save all the nodes in a table so that the mapping function can safely
+    -- modify the `next` pointers without breaking the traversal.
+    local given_nodes = {}
+    for n in self.node.traverse(head) do
+        insert(given_nodes, n)
+    end
+
+    -- Apply the function to each node
+    local out_nodes = {}
+    for _, n in ipairs(given_nodes) do
+        if is == true or self.node:is(n, is) then
+            append(out_nodes, { func(n) })
+        else
+            insert(out_nodes, n)
+        end
+    end
+
+    return self.node:join(out_nodes)
+end
+
+
+--= \subsection{\typ{luatools.node:colour_list}}
+--=
+--= Colours a list of nodes.
+
+--- @param  name string The name of the colour
+--- @return string? -   The colour's value, in a format-dependent format
+local function colour_name_to_value(name)
+    if lt.fmt.context then
+        return name
+    elseif lt.fmt.optex then
+        local colour = token.get_macro(name)
+        if not colour then
+            return
+        end
+
+        local rgb = colour:match[[\_setrgbcolor {([%d.]+ [%d.]+ [%d.]+)}]]
+        local cmyk = colour:match(
+            [[\_setcmykcolor {([%d.]+ [%d.]+ [%d.]+ [%d.]+)}]]
+        )
+
+        if rgb then
+            return ("%s rg %s RG"):format(rgb, rgb)
+        elseif cmyk then
+            return ("%s k %s K"):format(cmyk, cmyk)
+        end
+    elseif lt.fmt.plain or lt.fmt.latex then
+        -- `color`, `xcolor`
+        local colour = token.get_macro([[\color@]] .. name)
+
+        if colour then
+            local xcolor = colour:match[[\xcolor@ {}{([^}]+)}]]
+
+            return xcolor or colour
+        end
+
+        -- `l3color`
+        -- TODO Not implemented yet
+    end
+end
+
+
 --=---------------------------
 --= \section{Finalization} ---
 --=---------------------------
 
+--= Return the module table so that \lua{local luatools = require "luatools"}
+--= works as expected.
 return luatools
